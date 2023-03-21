d3f108f88eeaa77a8e13613735da5ac5e7a87bc9
oscar-jesus <oscarperniamoreno@gmail.com>
2023-03-21 00:54:59 -0400

    settings of the port of the http server and db connection

---
+++ settings/settings.go
@@ -0,0 +1,32 @@
+package settings
+
+import (
+	_ "embed"
+
+	"gopkg.in/yaml.v3"
+)
+
+//go:embed settings.yaml
+var settingsFile []byte
+
+type DatabaseConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Name     string `yaml:"name"`
+}
+
+type Settings struct {
+	Port int            `yaml:"port"`
+	DB   DatabaseConfig `yaml:"database"`
+}
+
+func New() (*Settings, error) {
+	s := &Settings{}
+	err := yaml.Unmarshal(settingsFile, s)
+	if err != nil {
+		return nil, err
+	}
+	return s, nil
+}
