7b76986ee384e0ad3127b52b7a87ca13b51d3434
oscar-jesus <oscarperniamoreno@gmail.com>
2023-03-21 22:49:50 -0400

    service for AddUserRole and RemoveUserRole

--- internal/service/service.go
+++ internal/service/service.go
@@ -9,10 +9,13 @@ import (
 
 // Service representa a la logica de negocios de la aplicacion
 //
-//go:generate mockery --name=Service --output=service --inpackage=true
+//go:generate mockery --name=Service
 type Service interface {
 	RegisterUser(ctx context.Context, email, name, password string) error
 	LoginUser(ctx context.Context, email, password string) (*models.User, error)
+
+	AddUserRole(ctx context.Context, userId string, roleId int) error
+	RemoveUserRole(ctx context.Context, userId string, roleId int) error
 }
 
 type serv struct {
--- internal/service/users.service.go
+++ internal/service/users.service.go
@@ -11,6 +11,8 @@ import (
 var (
 	errUserAlreadyExists  = errors.New("user already exists")
 	errInvalidCredentials = errors.New("invalid credentials")
+	errUserRoleAdded      = errors.New("user role already added")
+	errUserRoleNotFound   = errors.New("user role not found")
 )
 
 func (s *serv) RegisterUser(ctx context.Context, email, name, password string) error {
@@ -39,4 +41,35 @@ func (s *serv) LoginUser(ctx context.Context, email, password string) (*models.U
 		return nil, errInvalidCredentials
 	}
 	return &models.User{Id: u.Id, Name: u.Name, Email: u.Email}, nil
+}
+
+func (s *serv) AddUserRole(ctx context.Context, userId string, roleId int) error {
+	userRoles, err := s.repo.GetUserRoles(ctx, userId)
+	if err != nil {
+		return err
+	}
+	for _, r := range userRoles {
+		if roleId == r.RoleId {
+			return nil
+		}
+	}
+	return s.repo.SaveUserRole(ctx, userId, roleId)
+}
+
+func (s *serv) RemoveUserRole(ctx context.Context, userId string, roleId int) error {
+	userRoles, err := s.repo.GetUserRoles(ctx, userId)
+	if err != nil {
+		return err
+	}
+	roleExist := false
+	for _, r := range userRoles {
+		if roleId == r.RoleId {
+			roleExist = true
+			break
+		}
+	}
+	if !roleExist {
+		return errUserRoleNotFound
+	}
+	return s.repo.DeleteUserRole(ctx, userId, roleId)
 }
