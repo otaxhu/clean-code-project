755fa3bc8b07b7f7a1581199c9003e273aadb934
oscar-jesus <oscarperniamoreno@gmail.com>
2023-03-21 22:41:04 -0400

    create table roles and user_roles

--- scheme.sql
+++ scheme.sql
@@ -4,5 +4,20 @@ USE clean_code;
 CREATE TABLE users (
     id VARCHAR(255) PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+CREATE TABLE roles (
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+CREATE TABLE user_roles (
+    id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    role_id int NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+INSERT INTO roles (name) VALUES ("admin");
+INSERT INTO roles (name) VALUES ("seller");
+INSERT INTO roles (name) VALUES ("customer");
