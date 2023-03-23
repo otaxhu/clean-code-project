abd5ec789bb91edabdcbb7fbd5be0ef9c1f4abf4
oscar-jesus <oscarperniamoreno@gmail.com>
2023-03-22 22:40:40 -0400

    NewRepoMongo implementation of Repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/jmoiron/sqlx"
 	"github.com/otaxhu/clean-code-project/internal/entity"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // Repository representa las operaciones CRUD de la aplicacion
@@ -12,17 +13,27 @@ import (
 //go:generate mockery --name=Repository
 type Repository interface {
 	SaveUser(ctx context.Context, email, name, password string) error
+	DeleteUser(ctx context.Context, userId string) error
 	GetUserByEmail(ctx context.Context, email string) (*entity.User, error)
+	GetUserById(ctx context.Context, userId string) (*entity.User, error)
 
 	SaveUserRole(ctx context.Context, userId string, roleId int) error
 	DeleteUserRole(ctx context.Context, userId string, roleId int) error
 	GetUserRoles(ctx context.Context, userId string) ([]entity.UserRole, error)
 }
 
-type repo struct {
+type repoMysql struct {
 	db *sqlx.DB
 }
 
-func New(db *sqlx.DB) Repository {
-	return &repo{db: db}
+type repoMongo struct {
+	client *mongo.Client
+}
+
+func NewRepoMysql(db *sqlx.DB) Repository {
+	return &repoMysql{db: db}
+}
+
+func NewRepoMongo(client *mongo.Client) Repository {
+	return &repoMongo{client: client}
 }
