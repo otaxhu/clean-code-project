c60fb9eb0a3d574163c70b4596e2c6bd120c7ee5
oscar-jesus <oscarperniamoreno@gmail.com>
2023-03-24 17:57:28 -0400

    Roles field

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,8 +1,11 @@
 package entity
 
 type User struct {
-	Id       string `db:"id"`
-	Email    string `db:"email"`
-	Name     string `db:"name"`
-	Password string `db:"password"`
+	Id       string `db:"id" bson:"_id"`
+	Email    string `db:"email" bson:"email"`
+	Name     string `db:"name" bson:"name"`
+	Password string `db:"password" bson:"password"`
+
+	// This field is only for mongo, for sql DB use the UserRole struct that have his own table
+	Roles []UserRole `db:"-" bson:"roles"`
 }
